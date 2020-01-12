3daf5462b4f3b8fe75ddccbecf7b82a1161e6929
Mikel Bober-Irizar <mikel.cat7@gmail.com>
2020-01-12 16:10:17 +0000

    Native UI ROM Loader

--- src/com/mxbi/chip8/Main.java
+++ src/com/mxbi/chip8/Main.java
@@ -1,15 +1,33 @@
 package com.mxbi.chip8;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException, InvocationTargetException {
-	   short[] rom = ROMLoader.loadRomFromFile("pong.ch8");
+    	JFrame frame = new JFrame("CHIPSTER: Select a CHIP-8 ROM");
+//	    JFileChooser fc = new JFileChooser("./roms");
 
-	   SwingDisplay display = new SwingDisplay();
-	   CPU cpu = new CPU(rom, display, display);
-	   cpu.run();
+	    FileDialog fd = new FileDialog(frame, "CHIPSTER: Select a CHIP-8 ROM", FileDialog.LOAD);
+	    fd.setDirectory("roms");
+	    fd.setFilenameFilter((dir, name) -> name.endsWith(".ch8") || name.endsWith(".rom"));
+	    fd.setVisible(true);
+	    String filename = fd.getFile();
+
+//	    int returnVal = fc.showDialog(frame, "Load ROM");
+//	    frame.dispose();
+//	    if (returnVal == JFileChooser.APPROVE_OPTION) {
+//		    short[] rom = ROMLoader.loadRomFromFile(fc.getSelectedFile().getAbsolutePath());
+		if (filename != null) {
+	        short[] rom = ROMLoader.loadRomFromFile("roms/" + filename);
+
+		    SwingDisplay display = new SwingDisplay();
+
+		    CPU cpu = new CPU(rom, display, display);
+		    cpu.run();
+	    }
     }
 }
--- src/com/mxbi/chip8/ROMLoader.java
+++ src/com/mxbi/chip8/ROMLoader.java
@@ -1,5 +1,6 @@
 package com.mxbi.chip8;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
